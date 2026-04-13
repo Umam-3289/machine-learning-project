5018892e4de87b3175d5c35df8e701ae81f3bea7
Umam-3289 <umamkhan9931@gmail.com>
2026-04-13 14:01:29 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="Ml_project",
+    version='0.0.1',
+    author='Umam',
+    author_email='Umamkhan9931@gmail.com',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=get_requirements('requirements.txt')
+)
