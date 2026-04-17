24637c41a2ba5844d26072ec0cd6b5adb462d7ca
Umam-3289 <umamkhan9931@gmail.com>
2026-04-17 17:41:04 +0530

    Data Transformation

--- src/exception.py
+++ src/exception.py
@@ -11,7 +11,7 @@ def error_meassage_detail(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().error_message(error_message)
+        super().__init__(error_message)
         self.error_message=error_meassage_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
